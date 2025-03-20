d9e75d40fb337181d6109a3e660e764a46b1e00a
uroojkhaan <uroojkhaan004@gmail.com>
2025-03-20 20:30:32 +0500

    Add files via upload
    " A simple Python program that calculates a user's age based on their birth year. It demonstrates basic user input handling and arithmetic operations. "

---
+++ age_calculator.py
@@ -0,0 +1,13 @@
+# Age calculator program :
+
+# Get the current year :
+current_year = 2025
+
+# Taking user input for birth year :
+birth_year = int(input("Enter your Birth Year : \n "))
+
+# Calculate age :
+age = current_year - birth_year
+
+# Display the results :
+print(f" You are {age} years old ! ")
